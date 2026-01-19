e226f4c25bd2249d5014b11fd26edb142ef61612
prayudahlah <yudafihan@gmail.com>
2026-01-20 01:19:42 +0700

    fix(db): add unique constraint to genres, prod companies, and regions

--- db/postgres/showflix/ddl/catalogue/genres.sql
+++ db/postgres/showflix/ddl/catalogue/genres.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS catalogue.genres (
     genre_id   SERIAL
         PRIMARY KEY,
     genre_name VARCHAR(255) NOT NULL
+        CONSTRAINT genres_genre_name_uq
+            UNIQUE
 );
 
 ALTER TABLE catalogue.genres
