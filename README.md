cfad06f4f9603881edcc797a678bc9bde99d96f2
prayudahlah <yudafihan@gmail.com>
2026-01-30 20:51:36 +0700

    feat(garage): add script to initialize garage and manage credentials
    garage-init script ran by garage-init service in docker-compose
    script will add access key credentials to .env.garage

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # showflix2
+
+## setup
+
+#### setup environment
+```bash
+cp .env.example .env
+```
+
+fill the missing variables such as passwords and token. Use `openssl rand -hex 32` for `GARAGE_RPC_SECRET`
+
+#### run docker
+```bash
+docker-compose up --build
+```
---
+++ garage/garage-admin-tools/garage-init.sh
@@ -0,0 +1,168 @@
+#!/bin/sh
+
+set -e
+
+# fail-fast if env vars hasn't been set
+: "${GARAGE_ADMIN_TOKEN:?GARAGE_ADMIN_TOKEN is not set}"
+
+# Get current layout version
+LAYOUT_VERSION=$(curl -sf "$GARAGE_ADMIN_API/v2/GetClusterLayout" \
+    -H "Authorization: Bearer $GARAGE_ADMIN_TOKEN" |
+    jq -r '.version')
+
+# Only run if layout version is 0
+if [ "$LAYOUT_VERSION" -ne "0" ]; then
+    echo "Garage layout already initialized. Skipping."
+    exit 0
+fi
+
+echo "Garage layout not initialized. Proceeding..."
+
+# Get node id
+NODE_ID=""
+RETRIES=12
+SLEEP=5
+i=0
+
+while [ -z "$NODE_ID" ] || [ "$NODE_ID" = "null" ]; do
+    NODE_ID=$(curl -sf "$GARAGE_ADMIN_API/v2/GetClusterStatus" \
+        -H "Authorization: Bearer $GARAGE_ADMIN_TOKEN" |
+        jq -r '.nodes[0].id') || true
+
+    if [ -n "$NODE_ID" ] && [ "$NODE_ID" != "null" ]; then
+        break
+    fi
+
+    i=$((i + 1))
+    if [ "$i" -ge "$RETRIES" ]; then
+        echo "NODE_ID not found after $((RETRIES * SLEEP)) seconds. Exiting."
+        exit 1
+    fi
+
+    echo "Waiting for Garage node to be ready..."
+    sleep $SLEEP
+done
+if [ -z "$NODE_ID" ] || [ "$NODE_ID" = "null" ]; then
+    echo "Failed to get NODE_ID from cluster. Exiting."
+    exit 1
+fi
+
+# Create single node layout
+echo "initializing single node layout..."
+RESPONSE=$(
+    curl -s -w "\n%{http_code}" -X POST "$GARAGE_ADMIN_API/v2/UpdateClusterLayout" \
+        -H "Authorization: Bearer $GARAGE_ADMIN_TOKEN" \
+        -H "Content-Type: application/json" \
+        -d @- <<EOF
+{
+    "roles": [
+        {
+            "id": "$NODE_ID",
+            "zone": "dc1",
+            "capacity": $GARAGE_NODE_CAPACITY,
+            "tags": []
+        }
+    ]
+}
+EOF
+)
+
+HTTP_STATUS=$(echo "$RESPONSE" | tail -n1)
+BODY=$(echo "$RESPONSE" | sed '$d')
+
+if [ "$HTTP_STATUS" -ne 200 ]; then
+    echo "Failed to initialize single node layout. HTTP Status: $HTTP_STATUS"
+    echo "Response: $BODY"
+    exit 1
+fi
+
+echo "Succesfully staged single node layout. Applying layout..."
+
+# Apply layout
+RESPONSE=$(
+    curl -s -w "\n%{http_code}" -X POST "$GARAGE_ADMIN_API/v2/ApplyClusterLayout" \
+        -H "Authorization: Bearer $GARAGE_ADMIN_TOKEN" \
+        -H "Content-Type: application/json" \
+        -d @- <<EOF
+{
+    "version": 1
+}
+EOF
+)
+
+HTTP_STATUS=$(echo "$RESPONSE" | tail -n1)
+BODY=$(echo "$RESPONSE" | sed '$d')
+
+if [ "$HTTP_STATUS" -ne 200 ]; then
+    echo "Failed to apply layout changes. HTTP Status: $HTTP_STATUS"
+    echo "Response: $BODY"
+    exit 1
+fi
+
+echo "Succefully initialized single node layout."
+
+# Create access key
+echo "Creating access key..."
+
+RESPONSE=$(
+    curl -s -w "\n%{http_code}" -X POST "$GARAGE_ADMIN_API/v2/CreateKey" \
+        -H "Authorization: Bearer $GARAGE_ADMIN_TOKEN" \
+        -H "Content-Type: application/json" \
+        -d @- <<EOF
+{
+    "name": "airflow_key",
+    "neverExpires": true
+}
+EOF
+)
+
+HTTP_STATUS=$(echo "$RESPONSE" | tail -n1)
+BODY=$(echo "$RESPONSE" | sed '$d')
+
+if [ "$HTTP_STATUS" -ne 200 ]; then
+    echo "Failed to create access key. HTTP Status: $HTTP_STATUS"
+    echo "Response: $BODY"
+    exit 1
+fi
+
+GARAGE_ENV_PATH=/mnt/garage/.env.garage
+
+touch $GARAGE_ENV_PATH
+
+ACCESS_KEY_ID=$(echo "$BODY" | jq -r ".accessKeyId")
+SECRET_ACCESS_KEY=$(echo "$BODY" | jq -r ".secretAccessKey")
+
+# Write to .env.garage
+echo "GARAGE_ACCESS_KEY_ID=$ACCESS_KEY_ID" >"$GARAGE_ENV_PATH"
+echo "GARAGE_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY" >>"$GARAGE_ENV_PATH"
+
+echo "Succefully created access key."
+echo "Access key id and secret access key written to ./garage/.env.garage"
+
+# Create bucket
+echo "Creating garage bucket..."
+
+RESPONSE=$(
+    curl -s -w "\n%{http_code}" -X POST "$GARAGE_ADMIN_API/v2/CreateBucket" \
+        -H "Authorization: Bearer $GARAGE_ADMIN_TOKEN" \
+        -H "Content-Type: application/json" \
+        -d @- <<EOF
+{
+    "localAlias": {
+        "accessKeyId": "$ACCESS_KEY_ID",
+        "alias": "$GARAGE_BUCKET_NAME"
+    }
+}
+EOF
+)
+
+HTTP_STATUS=$(echo "$RESPONSE" | tail -n1)
+BODY=$(echo "$RESPONSE" | sed '$d')
+
+if [ "$HTTP_STATUS" -ne 200 ]; then
+    echo "Failed to create bucket. HTTP Status: $HTTP_STATUS"
+    echo "Response: $BODY"
+    exit 1
+fi
+
+echo "Succesfully created bucket with local alias: $GARAGE_BUCKET_NAME."
