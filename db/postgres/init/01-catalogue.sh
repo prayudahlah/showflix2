036a2643c6852f5cb10072927246a1b64ea26363
prayudahlah <yudafihan@gmail.com>
2026-02-04 21:25:44 +0700

    fix(db): remove original_language reference

--- db/postgres/init/01-catalogue.sh
+++ db/postgres/init/01-catalogue.sh
@@ -99,26 +99,26 @@ psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" 
             CONSTRAINT production_companies_production_company_name_uq UNIQUE
     );
 
-    CREATE TABLE IF NOT EXISTS catalogue.shows (
-        show_id              BIGINT NOT NULL PRIMARY KEY,
-        title                TEXT,
-        original_title       TEXT,
-        original_language_id UUID REFERENCES catalogue.languages,
-        vote_average         NUMERIC(5, 3),
-        vote_count           INTEGER,
-        popularity           NUMERIC(10, 4),
-        status               show_status_enum NOT NULL,
-        release_date         DATE,
-        budget               BIGINT,
-        revenue              BIGINT,
-        runtime              INTEGER,
-        is_adult             BOOLEAN,
-        backdrop_path        VARCHAR(255),
-        poster_path          VARCHAR(255),
-        homepage             TEXT,
-        tagline              TEXT,
-        overview             TEXT,
-        keywords             TEXT
+    CREATE TABLE catalogue.shows (
+        show_id           BIGINT NOT NULL PRIMARY KEY,
+        title             TEXT,
+        original_title    TEXT,
+        original_language VARCHAR(255),
+        vote_average      NUMERIC(5, 3),
+        vote_count        INTEGER,
+        popularity        NUMERIC(10, 4),
+        status            show_status_enum NOT NULL,
+        release_date      DATE,
+        budget            BIGINT,
+        revenue           BIGINT,
+        runtime           INTEGER,
+        is_adult          BOOLEAN,
+        backdrop_path     VARCHAR(255),
+        poster_path       VARCHAR(255),
+        homepage          TEXT,
+        tagline           TEXT,
+        overview          TEXT,
+        keywords          TEXT
     );
 
     CREATE TABLE IF NOT EXISTS catalogue.show_genres (
